475631d54d0e5ce4638826271fa4e5b1cae44608
Ganesh <ganeshdadi64@gmail.com>
2025-01-13 11:30:42 +0530

    changes moved

---
+++ userService20aug-main/src/main/java/com/scaler/userservice/models/Role.java
@@ -0,0 +1,15 @@
+package com.scaler.userservice.models;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@JsonDeserialize(as = Role.class)
+public class Role extends BaseModel {
+    private String role;
+    int val=10;
+}
